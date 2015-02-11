0395d2578a63a1951238d6960a0f712048ae6522
Gabriel Ivanica <ivanicagabriel@gmail.com>
2015-02-11 18:43:20 +0200

    [quiz page] updated style and functionality

--- wouso/resources/static/js/script.js
+++ wouso/resources/static/js/script.js
@@ -1,3 +1,32 @@
-$( document ).ready(function() {
-	console.log( "works!" );
-});
+var onPageReady = function onPageReady() {
+	setUpQuizPageEvents();
+};
+
+window.addEventListener('load', onPageReady);
+
+/*
+ * Quiz List Page
+ */
+var setUpQuizPageEvents = function setUpQuizPageEvents() {
+
+	var quizList = document.getElementById('quiz-list');
+
+	var handleClick = function handleClick(e) {
+		if (e.target.hasAttribute('data-toggle')) {
+			console.log(e.target);
+			var score = e.target.parentElement.querySelector('.score');
+			if (score.hasAttribute('data-visible')) {
+				score.removeAttribute('data-visible');
+			}
+			else
+				score.setAttribute('data-visible', '');
+		};
+
+	};
+
+	quizList.addEventListener('click', handleClick);
+
+};
+
+
+
