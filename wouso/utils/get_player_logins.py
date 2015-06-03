26fa880b5a127996f1258630d1f31aeff46dc2a2
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2015-06-03 14:13:01 +0300

    utils: Add script for getting player logins

---
+++ wouso/utils/get_player_logins.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# To test, run from parent folder using a command such as:
+# PYTHONPATH=../:. python utils/get_player_logins.py
+
+import codecs
+import sys
+import os
+
+UTF8Writer = codecs.getwriter('utf8')
+sys.stdout = UTF8Writer(sys.stdout)
+
+# Setup Django environment.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wouso.settings")
+
+from django.contrib.auth.models import User
+from wouso.core.user.models import Race
+from wouso.core.user.models import Player
+from wouso.interface.activity.models import Activity
+
+def main():
+    login_activities = Activity.objects.filter(action='login')
+    for a in login_activities:
+        player = a.user_from
+        print '"%s","%s","%s"' %(str(a.timestamp), player.race.title, player.full_name)
+
+if __name__ == "__main__":
+    sys.exit(main())
